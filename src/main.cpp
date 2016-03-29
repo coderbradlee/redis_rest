18caa67a1315784c3eb41571b5793474f255a706
lzxm160 <lzxm160@sina.com>
2016-03-29 11:58:46 +0800

    test cluster

--- src/main.cpp
+++ src/main.cpp
@@ -18,37 +18,34 @@ int main()
 {
 	try
 	{
+	
+		//HTTP-server at port 8080 using 4 threads
+		HttpServer server(get_config->m_webserver_port, get_config->m_threads);
+		//serverResource(server);
+		serverRedisResource(server, get_config->m_redis_host, get_config->m_redis_port, get_config->m_redis_password, get_config->m_webserver_url);
+		thread server_thread([&server]() {
+			//Start server
+			server.start();
+		});
+
+
 		{
-			//HTTP-server at port 8080 using 4 threads
-			HttpServer server(get_config->m_webserver_port, get_config->m_threads);
-			//serverResource(server);
-			serverRedisResource(server, get_config->m_redis_host, get_config->m_redis_port, get_config->m_redis_password, get_config->m_webserver_url);
-			thread server_thread([&server]() {
-				//Start server
-				server.start();
-			});
-
-
-{
-	this_thread::sleep_for(chrono::seconds(3));
-	HttpClient client("localhost:8888");
-    
-  { string json="{\"operation\": \"LIST_TRADETERM_BY_KEYWORDS\",\
-				\"requestData\": [ {\
-				\"keyword\":\"id\",\
-				\"value\":\"APOLLO-ALL\"}],\
-				\"requestor\": \"apollo-employee-portal\",\
-				\"requestTime\": \"2015-05-25 08:00:00\"\
-				}";
-   stringstream ss(json);    
-   auto r2=client.request("POST", "/apollo", ss);
-   cout << r2->content.rdbuf() << endl;
-  }
-}
-
-			
-
+			this_thread::sleep_for(chrono::seconds(3));
+			HttpClient client("localhost:8888");
+		    
+		  { string json="{\"operation\": \"LIST_TRADETERM_BY_KEYWORDS\",\
+						\"requestData\": [ {\
+						\"keyword\":\"id\",\
+						\"value\":\"APOLLO-ALL\"}],\
+						\"requestor\": \"apollo-employee-portal\",\
+						\"requestTime\": \"2015-05-25 08:00:00\"\
+						}";
+		   stringstream ss(json);    
+		   auto r2=client.request("POST", "/apollo", ss);
+		   cout << r2->content.rdbuf() << endl;
+		  }
 		}
+
 		{
 			boost::timer::cpu_timer pass;
 			pass.start();
